94f3bdf8a11bcc810fa2a9a540f0a6d60395906b
Mario Piazza <jaajaa3@gmail.com>
2016-06-08 11:29:23 +0200

    Module 3 completed

--- client/image_share.js
+++ client/image_share.js
@@ -1,7 +1,87 @@
-Template.images.helpers({
-  images: Images.find({}, {sort: {rating: -1, createdOn: -1}})
+// Routing
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
 });
 
+Router.route('/', function () {
+  this.render('welcome', {to: "main"});
+});
+
+Router.route('/images', function() {
+  this.render('navbar', {to: "navbar"});
+  this.render('images', {to: "main"});
+});
+
+Router.route('/image/:_id', function() {
+  this.render('navbar', {to: "navbar"});
+  this.render('image', {
+    to: "main",
+    data: function() {
+      return Images.findOne({_id: this.params._id});
+    }});
+});
+
+// Accounts config
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+// Infinite scrolling
+Session.set("imageLimit", 8);
+lastScrollTop = 0;
+$(window).scroll(function(event){
+  if ($(window).scrollTop()+ $(window).height() > $(document).height() - 100) {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > lastScrollTop) {
+      Session.set("imageLimit", Session.get("imageLimit") + 4);
+    }
+    lastScrollTop = scrollTop;
+  }
+});
+
+Template.images.helpers({
+  images: function() {
+    if (Session.get("userFilter")) {
+      return Images.find({createdBy: Session.get("userFilter")}, {sort: {rating: -1, createdOn: -1}});
+    } else {
+      return Images.find({}, {sort: {rating: -1, createdOn: -1}, limit: Session.get("imageLimit")});
+    }
+  },
+  filtering_images: function() {
+    if (Session.get("userFilter")) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  getFilterUser: function() {
+    if (Session.get("userFilter")) {
+      var user = Meteor.users.findOne({_id:Session.get("userFilter")});
+      if (user) {
+        return user.username;
+      }
+    }
+  },
+  getUser : function(user_id) {
+    var user = Meteor.users.findOne({_id: user_id});
+    if (user) {
+      return user.username;
+    } else {
+      return "Anonymous";
+    }
+  }
+});
+
+Template.body.helpers({
+  username: function() {
+    if (Meteor.user()) {
+      return Meteor.user().username;
+    }
+    else {
+      return "Anonymous";
+    }
+  }
+});
 Template.images.events({
   'click .js-image' : function() {
     $(event.target).css("width", "50px");
@@ -24,7 +104,15 @@ Template.images.events({
     Images.update(
       {_id : image_id},
       {$set: {rating:rating}}
-    );
+      );
+  },
+
+  'click .js-set-image-filter' : function(event) {
+    Session.set("userFilter", this.createdBy);
+  },
+
+  'click .js-unset-image-filter' : function(event) {
+    Session.set("userFilter", undefined);
   }
 });
 
@@ -33,11 +121,14 @@ Template.image_add_form.events({
     var img_src, img_alt;
     img_src = event.target.img_src.value;
     img_alt = event.target.img_alt.value;
-    Images.insert({
-      img_src: img_src,
-      img_alt: img_alt,
-      createdOn: new Date()
-    });
+    if (Meteor.user()) {
+      Images.insert({
+        img_src: img_src,
+        img_alt: img_alt,
+        createdOn: new Date(),
+        createdBy: Meteor.user()._id
+      });
+    }
     $("#image_add_form").modal('hide');
     return false;
   }
